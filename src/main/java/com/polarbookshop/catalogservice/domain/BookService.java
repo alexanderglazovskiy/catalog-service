73c709a7be9dd96d6cb6890a8cd4f5403370d7ea
aleksanderglazovskii <al.glazovskiy@gmail.com>
2024-10-30 21:14:23 +0300

    add some stuff

--- src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -17,7 +17,7 @@ public class BookService {
 
     public Book viewBookDetails(String isbn) {
         return bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new BookNotFoundException(isbn));
+            .orElseThrow(() -> new BookNotFoundException(isbn));
     }
 
     public Book addBookToCatalog(Book book) {
@@ -33,14 +33,19 @@ public class BookService {
 
     public Book editBookDetails(String isbn, Book book) {
         return bookRepository.findByIsbn(isbn)
-                .map(existingBook -> {
-                    var bookToUpdate = new Book(
-                            existingBook.isbn(),
-                            book.title(),
-                            book.author(),
-                            book.price());
-                    return bookRepository.save(bookToUpdate);
-                })
-                .orElseGet(() -> addBookToCatalog(book));
+            .map(existingBook -> {
+                var bookToUpdate = new Book(
+                    existingBook.id(),
+                    existingBook.isbn(),
+                    book.title(),
+                    book.author(),
+                    book.price(),
+                    existingBook.publisher(),
+                    existingBook.version(),
+                    existingBook.createdDate(),
+                    existingBook.lastModifiedDate());
+                return bookRepository.save(bookToUpdate);
+            })
+            .orElseGet(() -> addBookToCatalog(book));
     }
 }
